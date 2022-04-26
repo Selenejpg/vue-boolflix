9c6a4b9b031359a8817a53d144b1bcf677b840b3
noemipintaldi <noemipintaldi@gmail.com>
2022-04-26 19:53:25 +0200

    searhbar e logo

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,12 @@
 <template>
-  <header>
-   
+  <header class="d-flex p-3 flex-wrap justify-content-between">
+    <div class="">
+      <img class="logonetflix" src="../assets/Netflix_2015_logo.svg" alt=""> 
+    </div>
+    <div class="d-flex">
+      <input type="text" class="w-90 me-2 form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
   </header>
 </template>
 
@@ -12,5 +18,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
- /*Inserire style componente*/
+  .logonetflix{
+    width: 100px;
+  }
 </style>
